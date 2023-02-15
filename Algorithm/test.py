12048f244e41ef1d5ac154f2e0473c4d9cd4d1f1
RowenTey <kaiseong02@gmail.com>
2023-02-15 16:16:59 +0800

    Update test and remove redundant stuff

--- Algorithm/test.py
+++ Algorithm/test.py
@@ -5,8 +5,11 @@ from Grid.obstacle import Obstacle
 from Misc.positioning import Position
 from Misc.direction import Direction
 
-tmp = [[50, 50, Direction.TOP], [90, 90, Direction.BOTTOM],
-       [20, 80, Direction.BOTTOM], [120, 150, Direction.RIGHT]]
+tmp = [[30, 30, Direction.RIGHT], [60, 60, Direction.TOP],
+       [90, 110, Direction.LEFT], [20, 160, Direction.BOTTOM],
+       [100, 170, Direction.BOTTOM], [170, 170, Direction.LEFT],
+       [160, 40, Direction.BOTTOM], [130, 10, Direction.LEFT]]
+
 obstacles = []
 i = 0
 for x, y, direction in tmp:
@@ -15,12 +18,14 @@ for x, y, direction in tmp:
     i += 1
     obstacles.append(obstacle)
 
-print("list of obstacles: ")
-print(obstacles)
+# print("List of obstacles: ")
+# print(obstacles)
 grid = Grid(obstacles)
-print("list of target positions")
+# print("List of target positions")
+
 for x in grid.obstacles:
     print(x.target_position)
+
 robot = Robot(grid)
 test = Hamiltonian(grid=grid, robot=robot)
 test.plan_path()
