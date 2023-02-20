3a0a3fa623a9b5c830eafc7a16625425d81b8ebd
Gan Hao Yi <ganhaoyi.100@gmail.com>
2023-02-20 21:08:37 +0800

    fixed display skipping steps issue

--- Algorithm/simulation.py
+++ Algorithm/simulation.py
@@ -1101,7 +1101,6 @@ class Simulation():
         pygame.time.delay(550)
 
     def parseCmd(self, cmd):
-        self.updatingDisplay()
         if isinstance(cmd, StraightCommand):
             if (cmd.dist // 10) >= 0:
                 for i in range(cmd.dist // 10):
@@ -1116,6 +1115,7 @@ class Simulation():
                     self.updatingDisplay()
                     pygame.display.update()
         elif isinstance(cmd, TurnCommand):
+            self.updatingDisplay()
             if cmd.type_of_turn == TypeOfTurn.MEDIUM:
                 if cmd.right and not cmd.left and not cmd.reverse:
                     self.turnRight(constants.GRID_LENGTH * constants.SCALING_FACTOR,
@@ -1147,6 +1147,7 @@ class Simulation():
                            constants.SCALING_FACTOR, constants.RED, constants.ORANGE, constants.PINK)
         else:
             print("error!")
+        self.updatingDisplay()
         pygame.display.update()
 
     def runSimulation(self, bot):
@@ -1169,6 +1170,7 @@ class Simulation():
                         self.bot.hamiltonian.plan_path()
                         for cmd in self.bot.hamiltonian.commands:
                             self.parseCmd(cmd)
+                            pygame.display.update()
                     elif (650 < x < 650 + constants.BUTTON_LENGTH) and (450 < y < 450 + constants.BUTTON_WIDTH):
                         # print("*****Setting obstacles*****")
                         # self.obstacles = self.createObstacles(
